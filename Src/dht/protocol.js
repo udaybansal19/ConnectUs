64d18a79db877afffea334f1f5ac1ab9ec0f453a
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-07-07 09:52:46 +0530

    implemented routing table

---
+++ Src/dht/protocol.js
@@ -0,0 +1,17 @@
+
+
+export function ping( nodeId ) {
+    return;
+}
+
+export function store( nodeId ) {
+
+}
+
+export function findNode( nodeId ) {
+
+}
+
+export function findValue( nodeId ) {
+    
+}
--- Src/dht/routingTable.js
+++ Src/dht/routingTable.js
@@ -4,6 +4,9 @@
 // TODO: Switch to a tree based approach as given in 
 // section 2.4 for handling hughly unbalanced tree and bucket spliting.
 
+import { myUser } from '../index';
+import * as protocol from './protocol';
+
 var nodeBits = 160;
 var kBucketSize = 20;
 
@@ -13,15 +16,60 @@ var kBucketSize = 20;
 // most recently seen node at tail
 
 // if node.existsInBucket == true
-//     add node to tail //it is now most recently seen
-//     if node in correct bucket && bucket.size < k 
-//         add node to tail
-//     else if bucket is full
-//         ping node at head
-//             if head.node.ping == inactive
-//                 remove head node from bucket
-//                 add node to tail
-//             else
-//                 move head node to tail
-//                 discard the node
+//     move node to tail //it is now most recently seen
+// else 
+//      if bucket.size < k 
+//          add node to tail
+//      else 
+//          if head.node.ping == active
+//             move head node to tail
+//             discard the node
+//          else
+//             remove head node from bucket
+//             add node to tail
+
+var id = myUser.id;
+
+var routingTable = new Array(nodeBits);
+
+function updateTable( nodeId ) {
+    var bucket = id ^ nodeId;
+    var index = routingTable[bucket].indexOf(nodeId);
+
+    if( index != -1) {
+        
+        routingTable[bucket].splice(index, 1);
+        routingTable[bucket].push(nodeId);
+
+    } else {
+
+        if( routingTable[bucket].length < kBucketSize) {
+
+            routingTable[bucket].push(nodeId);
+
+        } else {
+         
+            var headNode = routingTable[bucket][0];
+
+            if( isActive(headNode) ) {
+
+                routingTable[bucket].shift();
+                routingTable[bucket].push(headNode);
+
+            } else {
+
+                routingTable[bucket].shift();
+                routingTable[bucket].push(nodeId);
+
+            }
+
+        }
+
+    }
+
+}
+
+function isActive( nodeId ) {
+    return protocol.ping(nodeId);
+}
 
